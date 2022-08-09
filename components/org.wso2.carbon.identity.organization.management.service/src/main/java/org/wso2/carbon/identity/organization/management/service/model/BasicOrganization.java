538d3f8a0fad50819d39264a2aee45ff7fe2658b
Sachin Mamoru <sachinmamoru@gmail.com>
2022-08-09 09:53:40 +0530

    resolved comments for the PR

--- components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/model/BasicOrganization.java
+++ components/org.wso2.carbon.identity.organization.management.service/src/main/java/org/wso2/carbon/identity/organization/management/service/model/BasicOrganization.java
@@ -60,10 +60,12 @@ public class BasicOrganization {
     }
 
     public String getStatus() {
+
         return status;
     }
 
     public void setStatus(String status) {
+
         this.status = status;
     }
 }
