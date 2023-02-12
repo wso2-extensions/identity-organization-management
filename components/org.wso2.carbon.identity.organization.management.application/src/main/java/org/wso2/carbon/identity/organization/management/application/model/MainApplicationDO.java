4e8dc26ee905a150339f53ca6fce470b70bb7b06
Sachin Mamoru <sachinmamoru@gmail.com>
2023-02-12 22:23:09 +0530

    adjusted the comments

--- components/org.wso2.carbon.identity.organization.management.application/src/main/java/org/wso2/carbon/identity/organization/management/application/model/MainApplicationDO.java
+++ components/org.wso2.carbon.identity.organization.management.application/src/main/java/org/wso2/carbon/identity/organization/management/application/model/MainApplicationDO.java
@@ -33,8 +33,8 @@ public class MainApplicationDO {
     }
 
     /**
-     * Get the organization id.
-     * @return String: organizationId.
+     * Get the Organization Id.
+     * @return {@link String} : OrganizationId.
      */
     public String getOrganizationId() {
 
@@ -42,8 +42,8 @@ public class MainApplicationDO {
     }
 
     /**
-     * Get the main application id.
-     * @return String: mainApplicationId
+     * Get the Main Application Id.
+     * @return {@link String} : MainApplicationId.
      */
     public String getMainApplicationId() {
 
