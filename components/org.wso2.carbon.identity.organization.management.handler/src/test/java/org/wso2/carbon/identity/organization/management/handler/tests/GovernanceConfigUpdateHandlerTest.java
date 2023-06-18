0d99cc5d4ec27af4fc6f7d9df97417deadb0dcb1
anjanasamindraperera <anjanasamindraperera@gmail.com>
2023-06-19 00:04:03 +0530

    address test related comments

--- components/org.wso2.carbon.identity.organization.management.handler/src/test/java/org/wso2/carbon/identity/organization/management/handler/tests/GovernanceConfigUpdateHandlerTest.java
+++ components/org.wso2.carbon.identity.organization.management.handler/src/test/java/org/wso2/carbon/identity/organization/management/handler/tests/GovernanceConfigUpdateHandlerTest.java
@@ -23,6 +23,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.event.IdentityEventException;
 import org.wso2.carbon.identity.event.event.Event;
@@ -38,6 +39,7 @@ import org.wso2.carbon.identity.organization.management.service.model.Organizati
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -63,17 +65,13 @@ public class GovernanceConfigUpdateHandlerTest {
         OrganizationManagementHandlerDataHolder.getInstance().setOrganizationManager(organizationManager);
     }
 
-    @Test
-    public void testHandleEventWithPostAddOrganizationEvent()
+    @Test(dataProvider = "organizationDataProvider")
+    public void testHandleEventWithPostAddOrganizationEvent(Organization organization, int depth, boolean expectedPass)
             throws IdentityEventException, OrganizationManagementException, IdentityGovernanceException {
-
-        // Create a sample organization.
-        Organization organization = new Organization();
-        organization.setId("orgId");
 
         // Mock the necessary methods.
         when(organizationManager.resolveTenantDomain(organization.getId())).thenReturn("sampleTenantDomain");
-        when(organizationManager.getOrganizationDepthInHierarchy(organization.getId())).thenReturn(1);
+        when(organizationManager.getOrganizationDepthInHierarchy(organization.getId())).thenReturn(depth);
 
         // Trigger the event.
         Map<String, Object> eventProperties = new HashMap<>();
@@ -82,9 +80,28 @@ public class GovernanceConfigUpdateHandlerTest {
         configUpdateHandler.handleEvent(event);
 
         // Verify that the necessary methods are called.
-        verify(organizationManager).resolveTenantDomain(organization.getId());
         verify(organizationManager).getOrganizationDepthInHierarchy(organization.getId());
-        verify(identityGovernanceService).updateConfiguration(Mockito.anyString(), Mockito.anyMap());
+        if (expectedPass) {
+            verify(organizationManager).resolveTenantDomain(organization.getId());
+            verify(identityGovernanceService).updateConfiguration(Mockito.anyString(), Mockito.anyMap());
+        } else {
+            verify(identityGovernanceService, never()).updateConfiguration(Mockito.anyString(), Mockito.anyMap());
+        }
+    }
+
+    @DataProvider(name = "organizationDataProvider")
+    public Object[][] organizationDataProvider() {
+
+        Organization organization1 = new Organization();
+        organization1.setId("orgId1");
+
+        Organization organization2 = new Organization();
+        organization2.setId("orgId2");
+
+        return new Object[][]{
+                {organization1, 0, false},
+                {organization2, 2, true}
+        };
     }
 }
 
