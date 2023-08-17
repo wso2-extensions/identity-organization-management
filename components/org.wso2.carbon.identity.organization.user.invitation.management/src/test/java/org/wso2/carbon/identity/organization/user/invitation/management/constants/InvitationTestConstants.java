019e806d7be416964b1ac01b9c72657764fdfd62
Shan Jayathilaka <shanchathusanda@gmail.com>
2023-08-17 15:23:33 +0530

    Add unit tests for organization invitation management

---
+++ components/org.wso2.carbon.identity.organization.user.invitation.management/src/test/java/org/wso2/carbon/identity/organization/user/invitation/management/constants/InvitationTestConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.organization.user.invitation.management.constants;
+
+/**
+ * Constants related to the test cases.
+ */
+public class InvitationTestConstants {
+
+    public static final String INV_01_INVITATION_ID = "23446012-18c7-4cd6-a757-16c39dbf4053";
+    public static final String INV_01_CONF_CODE = "ea19bc9e-dd0c-46d2-9d7e-7f6adc781041";
+    public static final String INV_01_UN = "bob@gmail.com";
+    public static final String INV_01_EMAIL = "bob@gmail.com";
+    public static final String INV_01_USER_ORG_ID = "89bbcfed-4db4-4731-9992-cce1361a2e7e";
+    public static final String INV_01_INV_ORG_ID = "a033565b-e8ad-4191-aa95-c64307de8ae7";
+    public static final String INV_02_INVITATION_ID = "d9929863-1e38-486f-af3a-d83a1757fcf3";
+    public static final String INV_02_CONF_CODE = "ddd2dcbc-63c7-471e-a5a6-8708214c29ea";
+    public static final String INV_02_UN = "sam@gmail.com";
+    public static final String INV_02_EMAIL = "sam@gmail.com";
+    public static final String INV_02_USER_ORG_ID = "8d94ff8a-031f-4719-8713-c6a9819b23b2";
+    public static final String INV_02_INV_ORG_ID = "dc828181-e1a8-4f5e-8936-f154f4aefa75";
+    public static final String INV_03_INVITATION_ID = "a26be854-e3ec-496d-b56a-959f634fbe91";
+    public static final String INV_03_CONF_CODE = "b64cd36a-5e65-4881-ab56-3a85030f9016";
+    public static final String INV_03_UN = "tom@gmail.com";
+    public static final String INV_03_EMAIL = "tom@gmail.com";
+    public static final String INV_03_USER_ORG_ID = "8d94ff8a-031f-4719-8713-c6a9819b23b2";
+    public static final String INV_03_INV_ORG_ID = "dc828181-e1a8-4f5e-8936-f154f4aefa75";
+}
