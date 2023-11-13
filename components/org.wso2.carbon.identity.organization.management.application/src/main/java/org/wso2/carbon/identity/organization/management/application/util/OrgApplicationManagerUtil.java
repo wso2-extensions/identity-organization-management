7a8bfe94e874e2c921a3e957b700b17f95a4fa58
dewniMW <dewni.matheesha@gmail.com>
2023-11-13 15:18:37 +0530

    Add SP property to persist if the SP is shared with sub orgs

--- components/org.wso2.carbon.identity.organization.management.application/src/main/java/org/wso2/carbon/identity/organization/management/application/util/OrgApplicationManagerUtil.java
+++ components/org.wso2.carbon.identity.organization.management.application/src/main/java/org/wso2/carbon/identity/organization/management/application/util/OrgApplicationManagerUtil.java
@@ -24,14 +24,18 @@ import org.wso2.carbon.identity.application.common.model.ServiceProviderProperty
 import org.wso2.carbon.identity.core.util.IdentityDatabaseUtil;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.wso2.carbon.identity.organization.management.application.constant.OrgApplicationMgtConstants.IS_APP_SHARED;
 import static org.wso2.carbon.identity.organization.management.application.constant.OrgApplicationMgtConstants.SHARE_WITH_ALL_CHILDREN;
 
 /**
  * This class provides utility functions for the Organization Application Management.
  */
 public class OrgApplicationManagerUtil {
+
+    private static final ThreadLocal<List<String>> b2bApplicationIds = new ThreadLocal<>();
 
     /**
      * Get a new Jdbc Template.
@@ -68,6 +72,64 @@ public class OrgApplicationManagerUtil {
 
             serviceProvider.setSpProperties(newSpProperties);
         }
+    }
+
+    /**
+     * Set property value to service provider indicating if the app is shared with any child organizations.
+     *
+     * @param serviceProvider The main application.
+     * @param value           The property value.
+     */
+    public static void setIsAppSharedProperty(ServiceProvider serviceProvider, boolean value) {
+
+        Optional<ServiceProviderProperty> appShared = Arrays.stream(serviceProvider.getSpProperties())
+                .filter(p -> IS_APP_SHARED.equals(p.getName()))
+                .findFirst();
+        if (appShared.isPresent()) {
+            appShared.get().setValue(Boolean.toString(value));
+        } else {
+            ServiceProviderProperty[] spProperties = serviceProvider.getSpProperties();
+            ServiceProviderProperty[] newSpProperties = new ServiceProviderProperty[spProperties.length + 1];
+            System.arraycopy(spProperties, 0, newSpProperties, 0, spProperties.length);
+
+            ServiceProviderProperty isAppSharedProperty = new ServiceProviderProperty();
+            isAppSharedProperty.setName(IS_APP_SHARED);
+            isAppSharedProperty.setValue(Boolean.toString(value));
+            newSpProperties[spProperties.length] = isAppSharedProperty;
+
+            serviceProvider.setSpProperties(newSpProperties);
+        }
+    }
+
+    /**
+     * Retrieve the B2B application IDs.
+     *
+     * @return B2B application IDs.
+     */
+    public static List<String> getB2BApplicationIds() {
+
+        if (b2bApplicationIds.get() == null) {
+            return null;
+        }
+        return b2bApplicationIds.get();
+    }
+
+    /**
+     * Sets the thread local value to persist the B2B application IDs.
+     *
+     * @param b2BApplicationIds The email verification state to be skipped.
+     */
+    public static void setB2BApplicationIds(List<String> b2BApplicationIds) {
+
+        OrgApplicationManagerUtil.b2bApplicationIds.set(b2BApplicationIds);
+    }
+
+    /**
+     * Clear the thread local used to persist the B2B application IDs.
+     */
+    public static void clearB2BApplicationIds() {
+
+        b2bApplicationIds.remove();
     }
 }
 
