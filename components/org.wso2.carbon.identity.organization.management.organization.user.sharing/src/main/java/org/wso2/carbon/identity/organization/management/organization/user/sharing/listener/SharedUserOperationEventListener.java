136ad789c862def79d3773cac868307d8e069e4e
AnuradhaSK <anuradha199528@gmail.com>
2024-10-14 20:38:35 +0530

    support editing shared user profile and return aggregated profile for shared user

--- components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/listener/SharedUserOperationEventListener.java
+++ components/org.wso2.carbon.identity.organization.management.organization.user.sharing/src/main/java/org/wso2/carbon/identity/organization/management/organization/user/sharing/listener/SharedUserOperationEventListener.java
@@ -19,22 +19,28 @@
 package org.wso2.carbon.identity.organization.management.organization.user.sharing.listener;
 
 import org.apache.commons.lang.StringUtils;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.identity.core.AbstractIdentityUserOperationEventListener;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.OrganizationUserSharingService;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.OrganizationUserSharingServiceImpl;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.internal.OrganizationUserSharingDataHolder;
+import org.wso2.carbon.identity.organization.management.organization.user.sharing.models.UserAssociation;
 import org.wso2.carbon.identity.organization.management.organization.user.sharing.util.OrganizationSharedUserUtil;
 import org.wso2.carbon.identity.organization.management.service.exception.OrganizationManagementException;
+import org.wso2.carbon.identity.organization.management.service.util.OrganizationManagementUtil;
+import org.wso2.carbon.user.api.UserRealm;
 import org.wso2.carbon.user.core.UserStoreClientException;
 import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.common.AbstractUserStoreManager;
+import org.wso2.carbon.user.core.service.RealmService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.wso2.carbon.identity.organization.management.organization.user.sharing.constant.UserSharingConstants.CLAIM_MANAGED_ORGANIZATION;
 import static org.wso2.carbon.identity.organization.management.service.constant.OrganizationManagementConstants.ErrorMessages.ERROR_CODE_MANAGED_ORGANIZATION_CLAIM_UPDATE_NOT_ALLOWED;
-import static org.wso2.carbon.identity.organization.management.service.constant.OrganizationManagementConstants.ErrorMessages.ERROR_CODE_SHARED_USER_CLAIM_UPDATE_NOT_ALLOWED;
 import static org.wso2.carbon.identity.organization.management.service.util.Utils.getOrganizationId;
 import static org.wso2.carbon.identity.organization.management.service.util.Utils.getTenantDomain;
 
@@ -49,7 +55,64 @@ public class SharedUserOperationEventListener extends AbstractIdentityUserOperat
     @Override
     public int getExecutionOrderId() {
 
-        return 128;
+        return 8;
+    }
+
+    public boolean doPostGetUserClaimValuesWithID(String userID, String[] claims, String profileName,
+                                                  Map<String, String> claimMap, UserStoreManager userStoreManager)
+            throws UserStoreException {
+
+        if (!isEnable() || userStoreManager == null) {
+            return true;
+        }
+        String currentTenantDomain = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantDomain();
+        String currentOrganizationId = PrivilegedCarbonContext.getThreadLocalCarbonContext().getOrganizationId();
+        try {
+            if (!OrganizationManagementUtil.isOrganization(currentTenantDomain)) {
+                // There is no shared users in root organizations. Hence, return.
+                return true;
+            }
+            UserAssociation userAssociation =
+                    OrganizationUserSharingDataHolder.getInstance().getOrganizationUserSharingService()
+                            .getUserAssociation(userID, currentOrganizationId);
+            if (userAssociation == null) {
+                // User is not a shared user. Hence, return.
+                return true;
+            }
+            // Get the associated user id and user's managed organization.
+            String associatedUserId = userAssociation.getAssociatedUserId();
+            String userResidentOrganizationId = userAssociation.getUserResidentOrganizationId();
+            String userResidentTenantDomain = OrganizationUserSharingDataHolder.getInstance().getOrganizationManager()
+                    .resolveTenantDomain(userResidentOrganizationId);
+
+            // Get the resident user claim values.
+            AbstractUserStoreManager residentUserStoreManager =
+                    getAbstractUserStoreManager(IdentityTenantUtil.getTenantId(userResidentTenantDomain));
+            Map<String, String> residentUserClaimValuesWithID =
+                    residentUserStoreManager.getUserClaimValuesWithID(associatedUserId, claims, profileName);
+            claimMap.putAll(getAggregatedProfile(claimMap, residentUserClaimValuesWithID));
+        } catch (OrganizationManagementException | org.wso2.carbon.user.api.UserStoreException e) {
+            throw new UserStoreException(e.getMessage(), e);
+        }
+        return true;
+    }
+
+    private Map<String, String> getAggregatedProfile(Map<String, String> sharedUserClaims,
+                                                     Map<String, String> residentUserClaims) {
+
+        if (sharedUserClaims == null) {
+            return residentUserClaims;
+        } else if (residentUserClaims == null) {
+            return sharedUserClaims;
+        }
+        Map<String, String> result =
+                new HashMap<>(residentUserClaims); // Start with all entries from residentUserClaims
+        result.remove("http://wso2.org/claims/groups");
+        result.remove("http://wso2.org/claims/roles");
+        for (Map.Entry<String, String> entry : sharedUserClaims.entrySet()) {
+            result.put(entry.getKey(), entry.getValue()); // Add entries from sharedUserClaims, overriding duplicates
+        }
+        return result;
     }
 
     @Override
@@ -86,7 +149,6 @@ public class SharedUserOperationEventListener extends AbstractIdentityUserOperat
         if (!isEnable() || userStoreManager == null) {
             return true;
         }
-        blockClaimUpdatesForSharedUser((AbstractUserStoreManager) userStoreManager, userID);
         if (!claims.isEmpty() && claims.containsKey(CLAIM_MANAGED_ORGANIZATION)) {
             throw new UserStoreClientException(
                     String.format(ERROR_CODE_MANAGED_ORGANIZATION_CLAIM_UPDATE_NOT_ALLOWED.getDescription(),
@@ -103,7 +165,6 @@ public class SharedUserOperationEventListener extends AbstractIdentityUserOperat
         if (!isEnable() || userStoreManager == null) {
             return true;
         }
-        blockClaimUpdatesForSharedUser((AbstractUserStoreManager) userStoreManager, userID);
         if (CLAIM_MANAGED_ORGANIZATION.equals(claimURI)) {
             throw new UserStoreClientException(
                     String.format(ERROR_CODE_MANAGED_ORGANIZATION_CLAIM_UPDATE_NOT_ALLOWED.getDescription(),
@@ -113,13 +174,11 @@ public class SharedUserOperationEventListener extends AbstractIdentityUserOperat
         return true;
     }
 
-    private void blockClaimUpdatesForSharedUser(AbstractUserStoreManager userStoreManager, String userID)
-            throws UserStoreException {
+    private AbstractUserStoreManager getAbstractUserStoreManager(int tenantId)
+            throws org.wso2.carbon.user.api.UserStoreException {
 
-        if (StringUtils.isEmpty(OrganizationSharedUserUtil.getUserManagedOrganizationClaim(userStoreManager, userID))) {
-            return;
-        }
-        throw new UserStoreClientException(ERROR_CODE_SHARED_USER_CLAIM_UPDATE_NOT_ALLOWED.getMessage(),
-                ERROR_CODE_SHARED_USER_CLAIM_UPDATE_NOT_ALLOWED.getCode());
+        RealmService realmService = OrganizationUserSharingDataHolder.getInstance().getRealmService();
+        UserRealm tenantUserRealm = realmService.getTenantUserRealm(tenantId);
+        return (AbstractUserStoreManager) tenantUserRealm.getUserStoreManager();
     }
 }
