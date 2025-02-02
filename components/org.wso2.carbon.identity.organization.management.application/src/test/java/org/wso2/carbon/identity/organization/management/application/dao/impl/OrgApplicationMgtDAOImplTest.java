94933dacd1a7d80ced7ea6368e36fc217e046198
DilshanSenarath <74205483+DilshanSenarath@users.noreply.github.com>
2025-02-02 17:14:10 +0530

    update the existing test class into scalable test class

--- components/org.wso2.carbon.identity.organization.management.application/src/test/java/org/wso2/carbon/identity/organization/management/application/dao/impl/OrgApplicationMgtDAOImplTest.java
+++ components/org.wso2.carbon.identity.organization.management.application/src/test/java/org/wso2/carbon/identity/organization/management/application/dao/impl/OrgApplicationMgtDAOImplTest.java
@@ -18,229 +18,217 @@
 
 package org.wso2.carbon.identity.organization.management.application.dao.impl;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.commons.lang.StringUtils;
+import org.mockito.MockedStatic;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.wso2.carbon.identity.core.util.IdentityDatabaseUtil;
+import org.wso2.carbon.CarbonConstants;
+import org.wso2.carbon.base.CarbonBaseConstants;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.identity.application.common.IdentityApplicationManagementException;
+import org.wso2.carbon.identity.application.common.model.ServiceProvider;
+import org.wso2.carbon.identity.application.mgt.dao.impl.ApplicationDAOImpl;
+import org.wso2.carbon.identity.application.mgt.internal.ApplicationManagementServiceComponentHolder;
+import org.wso2.carbon.identity.application.mgt.provider.ApplicationPermissionProvider;
+import org.wso2.carbon.identity.common.testng.WithH2Database;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.organization.management.application.model.SharedApplicationDO;
+import org.wso2.carbon.identity.organization.management.service.exception.OrganizationManagementException;
+import org.wso2.carbon.identity.organization.management.service.exception.OrganizationManagementServerException;
+import org.wso2.carbon.user.core.UserRealm;
+import org.wso2.carbon.user.core.common.AbstractUserStoreManager;
+import org.wso2.carbon.user.core.service.RealmService;
 
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.sql.DataSource;
-
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
+import static org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENANT_DOMAIN_NAME;
+import static org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENANT_ID;
 
 /**
  * Unit tests for OrgApplicationMgtDAOImpl.
  */
+@WithH2Database(jndiName = "jdbc/WSO2IdentityDB", files = {"dbscripts/identity.sql"})
 public class OrgApplicationMgtDAOImplTest {
 
-    private static final String DB_NAME = "testOrgAppMgtDB";
-    private static final String H2_SCRIPT_NAME = "h2.sql";
-    private static Map<String, BasicDataSource> dataSourceMap = new HashMap<>();
+    private static final String DEFAULT_USER_STORE_DOMAIN = "PRIMARY";
+    private static final String USERNAME = "test-user";
+    private static final String USER_ID = "test-user-id";
+    private static final String SUPER_ORG_ID = "test-super-org-id";
+
+    MockedStatic<IdentityTenantUtil> mockIdentityTenantUtil;
+    MockedStatic<IdentityUtil> mockIdentityUtil;
+    MockedStatic<ApplicationManagementServiceComponentHolder> mockedApplicationManagementServiceComponentHolder;
+    ApplicationManagementServiceComponentHolder mockComponentHolder;
+    UserRealm mockUserRealm;
+    RealmService mockRealmService;
+    AbstractUserStoreManager mockAbstractUserStoreManager;
+    ApplicationPermissionProvider mockApplicationPermissionProvider;
 
     private OrgApplicationMgtDAOImpl orgApplicationMgtDAO;
+    private ApplicationDAOImpl applicationDAO;
 
-    private static final String ROOT_APP_ID = "fa9b9ac5-a429-49e2-9c51-4259c7ebe45e";
-    private static final String ROOT_APP_NAME = "test-app";
-    private static final String ROOT_ORG_ID = "72b81cba-51c7-4dc1-91be-b267e177c17a";
-    private static final int ROOT_TENANT_ID = 1;
-    private static final String SHARED_APP_ID_1 = "42ef1d92-add6-449b-8a3c-fc308d2a4eac";
-    private static final String SHARED_ORG_ID_1 = "30b701c6-e309-4241-b047-0c299c45d1a0";
-    private static final int SHARED_TENANT_ID_1 = 2;
-    private static final String SHARED_APP_ID_2 = "1e2ef3df-e670-4339-9833-9df41dda7c96";
-    private static final String SHARED_ORG_ID_2 = "93d996f9-a5ba-4275-a52b-adaad9eba869";
-    private static final int SHARED_TENANT_ID_2 = 3;
-    private static final String UNSHARED_ORG_ID = "89d996f9-a5ba-4275-a52b-adaad9eba869";
-    private static final String PRIMARY_USERSTORE = "PRIMARY";
-    private static final String ADMIN_USERNAME = "admin";
-    private static final String AUTH_TYPE = "default";
-
-    private static final String CREATE_APPLICATION_SQL_STATEMENT = "INSERT INTO SP_APP " +
-            "(TENANT_ID, APP_NAME, USER_STORE, USERNAME, AUTH_TYPE, UUID) VALUES (?, ?, ?, ?, ?, ?);";
-    private static final String SHARE_APPLICATION_SQL_STATEMENT = "INSERT INTO SP_SHARED_APP " +
-            "(MAIN_APP_ID, OWNER_ORG_ID, SHARED_APP_ID, SHARED_ORG_ID, SHARE_WITH_ALL_CHILDREN) " +
-            "VALUES (?, ?, ?, ?, ?);";
-    private static final String DELETE_APPLICATION_SQL_STATEMENT = "DELETE FROM SP_APP " +
-            "WHERE UUID = ?;";
-    private static final String DELETE_SHARED_APPLICATION_SQL_STATEMENT = "DELETE FROM SP_SHARED_APP " +
-            "WHERE SHARED_APP_ID = ?;";
-
+    /**
+     * Setup the test environment for OrgApplicationMgtDAOImpl.
+     */
     @BeforeClass
-    public void setUp() throws Exception {
+    public void setup() throws org.wso2.carbon.user.api.UserStoreException, IdentityApplicationManagementException {
 
-        initiateH2Base();
-        mockDataSource();
-
-        // Create root application.
-        createApplication(ROOT_TENANT_ID, ROOT_APP_NAME, ROOT_APP_ID, PRIMARY_USERSTORE, ADMIN_USERNAME, AUTH_TYPE);
+        mockIdentityTenantUtil = mockStatic(IdentityTenantUtil.class);
+        mockIdentityUtil = mockStatic(IdentityUtil.class);
+        mockedApplicationManagementServiceComponentHolder =
+                mockStatic(ApplicationManagementServiceComponentHolder.class);
+        mockComponentHolder = mock(ApplicationManagementServiceComponentHolder.class);
+        mockUserRealm = mock(UserRealm.class);
+        mockRealmService = mock(RealmService.class);
+        mockAbstractUserStoreManager = mock(AbstractUserStoreManager.class);
+        mockApplicationPermissionProvider = mock(ApplicationPermissionProvider.class);
+        setupInitConfigurations();
 
         orgApplicationMgtDAO = new OrgApplicationMgtDAOImpl();
+        applicationDAO = new ApplicationDAOImpl();
     }
 
+    /**
+     * Clean up the test environment of OrgApplicationMgtDAOImpl.
+     */
     @AfterClass
-    public void tearDown() throws Exception {
+    public void tearDown() {
 
-        // Delete root application.
-        deleteApplication(ROOT_APP_ID);
-        closeH2Base();
+        mockIdentityTenantUtil.close();
+        mockIdentityUtil.close();
+        mockedApplicationManagementServiceComponentHolder.close();
     }
 
     @DataProvider(name = "filteredSharedApplicationsTestData")
-    public Object[][] getFilteredSharedApplicationsTestData() {
+    public Object[][] getFilteredSharedApplicationsTestData()
+            throws IdentityApplicationManagementException, OrganizationManagementException {
 
-        return new Object[][]{
+        String sharedOrgID1 = "30b701c6-e309-4241-b047-0c299c45d1a0";
+        String sharedOrgID2 = "93d996f9-a5ba-4275-a52b-adaad9eba869";
+        String unsharedOrgID = "89d996f9-a5ba-4275-a52b-adaad9eba869";
+
+        Integer[] appIDs = createAndShareApplication("test-app", new String[] {sharedOrgID1, sharedOrgID2});
+
+        return new Object[][] {
                 // Passing org ids of both shared apps only.
-                {Arrays.asList(SHARED_ORG_ID_1, SHARED_ORG_ID_2), 2},
+                {Arrays.asList(sharedOrgID1, sharedOrgID2), 2, appIDs[0]},
                 // Passing org ids of both shared apps and an unshared org id.
-                {Arrays.asList(SHARED_ORG_ID_1, SHARED_ORG_ID_2, UNSHARED_ORG_ID), 2},
+                {Arrays.asList(sharedOrgID1, sharedOrgID2, unsharedOrgID), 2, appIDs[0]},
                 // Passing org id of one shared app only.
-                {Collections.singletonList(SHARED_ORG_ID_1), 1},
+                {Collections.singletonList(sharedOrgID1), 1, appIDs[0]},
                 // Passing org id of shared app and an unshared org id.
-                {Arrays.asList(SHARED_ORG_ID_1, UNSHARED_ORG_ID), 1},
+                {Arrays.asList(sharedOrgID1, unsharedOrgID), 1, appIDs[0]},
                 // Passing an unshared org id only.
-                {Collections.singletonList(UNSHARED_ORG_ID), 0},
+                {Collections.singletonList(unsharedOrgID), 0, appIDs[0]},
                 // Passing an empty list
-                {Collections.emptyList(), 0},
+                {Collections.emptyList(), 0, appIDs[0]},
         };
     }
 
     @Test(dataProvider = "filteredSharedApplicationsTestData")
-    public void testGetFilteredSharedApplications(List<String> sharedOrgIds, int expectedNumOfApps) throws Exception {
+    public void testGetFilteredSharedApplications(List<String> sharedOrgIds, int expectedNumOfApps, int rootAppId)
+            throws Exception {
 
-        // Create shared application 1.
-        createApplication(SHARED_TENANT_ID_1, ROOT_APP_NAME, SHARED_APP_ID_1, PRIMARY_USERSTORE, ADMIN_USERNAME,
-                AUTH_TYPE);
-        shareApplication(ROOT_APP_ID, ROOT_ORG_ID, SHARED_APP_ID_1, SHARED_ORG_ID_1);
-
-        // Create shared  application 2.
-        createApplication(SHARED_TENANT_ID_2, ROOT_APP_NAME, SHARED_APP_ID_2, PRIMARY_USERSTORE, ADMIN_USERNAME,
-                AUTH_TYPE);
-        shareApplication(ROOT_APP_ID, ROOT_ORG_ID, SHARED_APP_ID_2, SHARED_ORG_ID_2);
-
+        String rootAppUUID = applicationDAO.getApplication(rootAppId).getApplicationResourceId();
         List<SharedApplicationDO> sharedApplications =
-                orgApplicationMgtDAO.getSharedApplications(ROOT_APP_ID, ROOT_ORG_ID, sharedOrgIds);
+                orgApplicationMgtDAO.getSharedApplications(rootAppUUID, SUPER_ORG_ID, sharedOrgIds);
 
         Assert.assertNotNull(sharedApplications);
         Assert.assertEquals(sharedApplications.size(), expectedNumOfApps);
-
-        // Delete shared applications.
-        deleteSharedApplication(SHARED_APP_ID_1);
-        deleteSharedApplication(SHARED_APP_ID_2);
     }
 
-    private void initiateH2Base() throws Exception {
+    /**
+     * Create and share an application with the given shared organization ids.
+     *
+     * @param appName      Application name.
+     * @param sharedOrgIds Shared organization ids.
+     * @return Application IDs of the created applications.
+     */
+    private Integer[] createAndShareApplication(String appName, String[] sharedOrgIds)
+            throws IdentityApplicationManagementException, OrganizationManagementException {
 
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUsername("username");
-        dataSource.setPassword("password");
-        dataSource.setUrl("jdbc:h2:mem:test" + DB_NAME);
-        dataSource.setTestOnBorrow(true);
-        dataSource.setValidationQuery("select 1");
-        try (Connection connection = dataSource.getConnection()) {
-            connection.createStatement().executeUpdate("RUNSCRIPT FROM '" + getFilePath(H2_SCRIPT_NAME) + "'");
+        ServiceProvider application = new ServiceProvider();
+        application.setApplicationName(appName);
+        application.setApplicationVersion("v1.0.0");
+        Integer[] createAppIds = new Integer[sharedOrgIds.length + 1];
+        int appId = applicationDAO.createApplication(application, SUPER_TENANT_DOMAIN_NAME);
+        createAppIds[0] = appId;
+        for (int i = 0; i < sharedOrgIds.length; i++) {
+            String sharedOrgId = sharedOrgIds[i];
+            mockIdentityTenantUtil.when(() -> IdentityTenantUtil.getTenantId(eq(sharedOrgId)))
+                    .thenReturn(i + 1);
+            ServiceProvider sharedApp = new ServiceProvider();
+            sharedApp.setApplicationName(appName);
+            sharedApp.setApplicationVersion("v1.0.0");
+            int sharedAppId = applicationDAO.createApplication(sharedApp, sharedOrgId);
+            orgApplicationMgtDAO.addSharedApplication(application.getApplicationResourceId(), SUPER_ORG_ID,
+                    sharedApp.getApplicationResourceId(), sharedOrgId, false);
+            createAppIds[i + 1] = sharedAppId;
         }
-        dataSourceMap.put(DB_NAME, dataSource);
+        return createAppIds;
     }
 
-    private String getFilePath(String fileName) {
+    private void deleteAllApplications(String[] sharedOrgIds)
+            throws IdentityApplicationManagementException, OrganizationManagementServerException {
 
-        if (StringUtils.isNotBlank(fileName)) {
-            return Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "dbscripts",
-                    fileName).toString();
-        }
-        throw new IllegalArgumentException("DB Script file name cannot be empty.");
-    }
-
-    private void mockDataSource() {
-
-        DataSource dataSource = dataSourceMap.get(DB_NAME);
-        mockStatic(IdentityDatabaseUtil.class);
-        when(IdentityDatabaseUtil.getDataSource()).thenReturn(dataSource);
-    }
-
-    private Connection getConnection() throws SQLException {
-
-        if (dataSourceMap.get(DB_NAME) != null) {
-            return dataSourceMap.get(DB_NAME).getConnection();
-        }
-        throw new RuntimeException("No datasource initiated for database: " + DB_NAME);
-    }
-
-    public void closeH2Base() throws Exception {
-
-        BasicDataSource dataSource = dataSourceMap.remove(DB_NAME);
-        if (dataSource != null) {
-            dataSource.close();
+        applicationDAO.deleteApplications(SUPER_TENANT_ID);
+        for (String sharedOrgId : sharedOrgIds) {
+            applicationDAO.deleteApplications(IdentityTenantUtil.getTenantId(sharedOrgId));
+            orgApplicationMgtDAO.deleteSharedAppLinks(sharedOrgId);
         }
     }
 
-    private void createApplication(int tenantId, String appName, String appId, String userstore, String username,
-                                   String authType) throws Exception {
+    /**
+     * Setup the configurations for the test.
+     */
+    private void setupInitConfigurations()
+            throws org.wso2.carbon.user.api.UserStoreException, IdentityApplicationManagementException {
 
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(CREATE_APPLICATION_SQL_STATEMENT);
-            statement.setInt(1, tenantId);
-            statement.setString(2, appName);
-            statement.setString(3, userstore);
-            statement.setString(4, username);
-            statement.setString(5, authType);
-            statement.setString(6, appId);
-            statement.execute();
-        }
-    }
+        String carbonHome = Paths.get(System.getProperty("user.dir"), "target", "test-classes", "repository").
+                toString();
+        System.setProperty(CarbonBaseConstants.CARBON_HOME, carbonHome);
+        System.setProperty(CarbonBaseConstants.CARBON_CONFIG_DIR_PATH, Paths.get(carbonHome, "conf").toString());
 
-    private void shareApplication(String rootAppId, String rootOrgId, String sharedAppId, String sharedOrgId) {
+        PrivilegedCarbonContext.startTenantFlow();
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(SUPER_TENANT_DOMAIN_NAME);
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(SUPER_TENANT_ID);
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setUsername(USERNAME);
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setUserId(USER_ID);
 
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(SHARE_APPLICATION_SQL_STATEMENT);
-            statement.setString(1, rootAppId);
-            statement.setString(2, rootOrgId);
-            statement.setString(3, sharedAppId);
-            statement.setString(4, sharedOrgId);
-            statement.setBoolean(5, true);
-            statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException("Error while sharing application: " + rootAppId + " with organization: " +
-                    sharedOrgId, e);
-        }
-    }
+        CarbonConstants.ENABLE_LEGACY_AUTHZ_RUNTIME = false;
 
-    private void deleteApplication(String appId) {
+        mockIdentityTenantUtil.when(() -> IdentityTenantUtil.getTenantId(eq(SUPER_TENANT_DOMAIN_NAME)))
+                .thenReturn(SUPER_TENANT_ID);
+        mockIdentityTenantUtil.when(() -> IdentityTenantUtil.getTenantId(SUPER_ORG_ID))
+                .thenReturn(SUPER_TENANT_ID);
+        mockIdentityTenantUtil.when(() -> IdentityTenantUtil.getTenantDomain(eq(SUPER_TENANT_ID)))
+                .thenReturn(SUPER_TENANT_DOMAIN_NAME);
 
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(DELETE_APPLICATION_SQL_STATEMENT);
-            statement.setString(1, appId);
-            statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException("Error while deleting application: " + appId, e);
-        }
-    }
+        mockIdentityUtil.when(IdentityUtil::getIdentityConfigDirPath)
+                .thenReturn(Paths.get(carbonHome, "conf", "identity").toString());
+        mockIdentityUtil.when(() -> IdentityUtil.extractDomainFromName(anyString()))
+                .thenReturn(DEFAULT_USER_STORE_DOMAIN);
+        mockIdentityUtil.when(IdentityUtil::getPrimaryDomainName).thenReturn(SUPER_TENANT_DOMAIN_NAME);
 
-    private void deleteSharedApplication(String appId) {
-
-        try (Connection connection = getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(DELETE_SHARED_APPLICATION_SQL_STATEMENT);
-            statement.setString(1, appId);
-            statement.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException("Error while deleting shared application: " + appId, e);
-        }
-
-        deleteApplication(appId);
+        mockedApplicationManagementServiceComponentHolder.when(
+                        ApplicationManagementServiceComponentHolder::getInstance)
+                .thenReturn(mockComponentHolder);
+        when(mockComponentHolder.getRealmService()).thenReturn(mockRealmService);
+        when(mockRealmService.getTenantUserRealm(SUPER_TENANT_ID)).thenReturn(mockUserRealm);
+        when(mockUserRealm.getUserStoreManager()).thenReturn(mockAbstractUserStoreManager);
+        when(mockComponentHolder.getApplicationPermissionProvider()).thenReturn(mockApplicationPermissionProvider);
+        when(mockApplicationPermissionProvider.loadPermissions(anyString())).thenReturn(new ArrayList<>());
     }
 }
