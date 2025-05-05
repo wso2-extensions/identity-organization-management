c45326132739837562d2dadc994886385a3e8e5d
Yasasr1 <yasasramanayaka@gmail.com>
2025-05-05 16:42:35 +0530

    Refactor organization.resource.hierarchy.traverse.service.

--- components/org.wso2.carbon.identity.organization.application.resource.hierarchy.traverse.service/src/main/java/org/wso2/carbon/identity/organization/application/resource/hierarchy/traverse/service/OrgAppResourceResolverService.java
+++ components/org.wso2.carbon.identity.organization.application.resource.hierarchy.traverse.service/src/main/java/org/wso2/carbon/identity/organization/application/resource/hierarchy/traverse/service/OrgAppResourceResolverService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,40 +16,40 @@
  * under the License.
  */
 
-package org.wso2.carbon.identity.organization.resource.hierarchy.traverse.service;
+package org.wso2.carbon.identity.organization.application.resource.hierarchy.traverse.service;
 
 import org.wso2.carbon.identity.organization.resource.hierarchy.traverse.service.exception.OrgResourceHierarchyTraverseException;
 import org.wso2.carbon.identity.organization.resource.hierarchy.traverse.service.strategy.AggregationStrategy;
 
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 /**
- * Provides a service interface to retrieve resources from an organization's hierarchy.
- * Supports traversal of both organization and application hierarchies using customizable
- * resource retrieval and aggregation strategies.
- * <p>
- * The service is designed for extensibility, allowing clients to define their own retrieval logic
- * and aggregation mechanisms via functional interfaces and strategy patterns.
+ * Service interface for resolving resources from the organization and application hierarchy.
+ * This interface provides methods to retrieve resources by traversing the hierarchy of a given
+ * organization and application.
  */
-public interface OrgResourceResolverService {
+public interface OrgAppResourceResolverService {
 
     /**
-     * Retrieves resources by traversing the hierarchy of a given organization.
+     * Retrieves resources by traversing the hierarchy of a given organization and application.
      *
      * @param organizationId      The unique identifier of the organization.
-     * @param resourceRetriever   A function that defines how to fetch a resource for a given organization ID.
-     *                            The function must return an {@link Optional<T>} containing the resource if found,
+     * @param applicationId       The unique identifier of the application within the organization.
+     * @param resourceRetriever   A bi-function that defines how to fetch a resource based on the
+     *                            organization and application IDs. The function must return an
+     *                            {@link Optional <T>} containing the resource if found,
      *                            or an empty {@link Optional<T>} if not.
      * @param aggregationStrategy A strategy defining how to aggregate resources retrieved from
      *                            different levels of the hierarchy.
      * @param <T>                 The type of the resource being retrieved and aggregated.
-     * @return An aggregated resource of type <T> obtained from the organization hierarchy.
+     * @return An aggregated resource of type <T> obtained from the organization and application hierarchy.
      * @throws OrgResourceHierarchyTraverseException If any errors occur during resource retrieval
      *                                               or aggregation.
      */
-    <T> T getResourcesFromOrgHierarchy(String organizationId,
-                                       Function<String, Optional<T>> resourceRetriever,
+    <T> T getResourcesFromOrgHierarchy(String organizationId, String applicationId,
+                                       BiFunction<String, String, Optional<T>> resourceRetriever,
                                        AggregationStrategy<T> aggregationStrategy)
             throws OrgResourceHierarchyTraverseException;
+
 }
