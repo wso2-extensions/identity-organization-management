efd66ff93eb26208c40d89d0a56bfdeb61184970
SujanSanjula96 <sanjulasujan@gmail.com>
2026-05-19 14:19:01 +0530

    Fix query issues

--- components/org.wso2.carbon.identity.organization.management.capability.governance/src/main/java/org/wso2/carbon/identity/organization/management/capability/governance/constant/GovernancePolicySQLConstants.java
+++ components/org.wso2.carbon.identity.organization.management.capability.governance/src/main/java/org/wso2/carbon/identity/organization/management/capability/governance/constant/GovernancePolicySQLConstants.java
@@ -27,47 +27,47 @@ public class GovernancePolicySQLConstants {
     public static final String INSERT_ORG_GOVERNANCE_POLICY =
             "INSERT INTO UM_ORG_GOVERNANCE_POLICY " +
                     "(UM_RESOURCE_TYPE, UM_CAPABILITY, UM_GOVERNING_ORG_ID, UM_POLICY) " +
-                    "VALUES (:UM_RESOURCE_TYPE, :UM_CAPABILITY, :UM_GOVERNING_ORG_ID, :UM_POLICY)";
+                    "VALUES (:UM_RESOURCE_TYPE;, :UM_CAPABILITY;, :UM_GOVERNING_ORG_ID;, :UM_POLICY;)";
 
     public static final String SELECT_ORG_GOVERNANCE_POLICIES_BY_GOVERNING_ORG =
             "SELECT UM_ID, UM_RESOURCE_TYPE, UM_CAPABILITY, UM_GOVERNING_ORG_ID, UM_POLICY " +
                     "FROM UM_ORG_GOVERNANCE_POLICY " +
-                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID";
+                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID;";
 
     public static final String SELECT_ORG_GOVERNANCE_POLICY_BY_KEY =
             "SELECT UM_ID, UM_RESOURCE_TYPE, UM_CAPABILITY, UM_GOVERNING_ORG_ID, UM_POLICY " +
                     "FROM UM_ORG_GOVERNANCE_POLICY " +
-                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID " +
-                    "AND UM_CAPABILITY = :UM_CAPABILITY " +
-                    "AND UM_RESOURCE_TYPE = :UM_RESOURCE_TYPE";
+                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID; " +
+                    "AND UM_CAPABILITY = :UM_CAPABILITY; " +
+                    "AND UM_RESOURCE_TYPE = :UM_RESOURCE_TYPE;";
 
     public static final String UPDATE_ORG_GOVERNANCE_POLICY =
             "UPDATE UM_ORG_GOVERNANCE_POLICY " +
-                    "SET UM_POLICY = :UM_POLICY " +
-                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID " +
-                    "AND UM_RESOURCE_TYPE = :UM_RESOURCE_TYPE " +
-                    "AND UM_CAPABILITY = :UM_CAPABILITY";
+                    "SET UM_POLICY = :UM_POLICY; " +
+                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID; " +
+                    "AND UM_RESOURCE_TYPE = :UM_RESOURCE_TYPE; " +
+                    "AND UM_CAPABILITY = :UM_CAPABILITY;";
 
     public static final String DELETE_ORG_GOVERNANCE_POLICY_BY_KEY =
             "DELETE FROM UM_ORG_GOVERNANCE_POLICY " +
-                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID " +
-                    "AND UM_RESOURCE_TYPE = :UM_RESOURCE_TYPE " +
-                    "AND UM_CAPABILITY = :UM_CAPABILITY";
+                    "WHERE UM_GOVERNING_ORG_ID = :UM_GOVERNING_ORG_ID; " +
+                    "AND UM_RESOURCE_TYPE = :UM_RESOURCE_TYPE; " +
+                    "AND UM_CAPABILITY = :UM_CAPABILITY;";
 
     // Org governance selected organizations.
 
     public static final String INSERT_ORG_GOVERNANCE_ORG_SELECTED =
             "INSERT INTO UM_ORG_GOVERNANCE_ORG_SELECTED (UM_POLICY_ID, UM_TARGET_ORG_ID) " +
-                    "VALUES (:UM_POLICY_ID, :UM_TARGET_ORG_ID)";
+                    "VALUES (:UM_POLICY_ID;, :UM_TARGET_ORG_ID;)";
 
     public static final String SELECT_ORG_GOVERNANCE_ORG_SELECTED_BY_POLICY =
             "SELECT UM_ID, UM_POLICY_ID, UM_TARGET_ORG_ID " +
                     "FROM UM_ORG_GOVERNANCE_ORG_SELECTED " +
-                    "WHERE UM_POLICY_ID = :UM_POLICY_ID";
+                    "WHERE UM_POLICY_ID = :UM_POLICY_ID;";
 
     public static final String DELETE_ORG_GOVERNANCE_ORG_SELECTED_BY_POLICY =
             "DELETE FROM UM_ORG_GOVERNANCE_ORG_SELECTED " +
-                    "WHERE UM_POLICY_ID = :UM_POLICY_ID";
+                    "WHERE UM_POLICY_ID = :UM_POLICY_ID;";
 
     private GovernancePolicySQLConstants() {
 
